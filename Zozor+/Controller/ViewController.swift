3868e833c94fdaad8cc6973da0d96afd0b05f8bb
William <william.desecot@hotmail.fr>
2018-09-01 13:21:43 +0200

    Adding constrains to memoryLabel

--- Zozor+/Controller/ViewController.swift
+++ Zozor+/Controller/ViewController.swift
@@ -12,7 +12,25 @@ class ViewController: UIViewController {
     
     // MARK: - Properties
     let operation = Calculator()
-    var memoryLabel = UILabel()
+    var memoryLabel: UILabel = {
+        let label = UILabel()
+        label.text = "I am the memory label"
+        label.textColor = .white
+        label.textAlignment = .left
+        label.font = UIFont.systemFont(ofSize: 30)
+        label.adjustsFontSizeToFitWidth = true
+        // Enable autolayout
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    //MARK: - Methods
+    fileprivate func setUpLayout() {
+        memoryLabel.bottomAnchor.constraint(equalTo: textView.bottomAnchor, constant: textView.frame.height - 10).isActive = true
+        memoryLabel.leftAnchor.constraint(equalTo: textView.leftAnchor, constant: 5).isActive = true
+        memoryLabel.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        memoryLabel.widthAnchor.constraint(equalToConstant: textView.frame.width - 20).isActive = true
+    }
 
 
     // MARK: - Outlets
@@ -20,17 +38,11 @@ class ViewController: UIViewController {
     @IBOutlet var numberButtons: [UIButton]!
     
     // MARK: - viewDidLoad()
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        
-        memoryLabel.frame = CGRect(x: 5, y: textView.frame.height - 50, width: textView.frame.width - 20, height: 50)
-        memoryLabel.textColor = .white
-        memoryLabel.textAlignment = .left
-        memoryLabel.font = UIFont.systemFont(ofSize: 30)
-        memoryLabel.adjustsFontSizeToFitWidth = true
+
         self.textView.addSubview(memoryLabel)
+        setUpLayout()
     }
     
 
